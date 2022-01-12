0c8d8e777c3d24d132e5c8a7fd44c15082b9cf2a
Fernandosousa9 <75530766+Fernandosousa9@users.noreply.github.com>
2022-01-12 11:56:09 -0300

    Update aposta.js

--- aposta.js
+++ aposta.js
@@ -1,17 +1,35 @@
 function escolheCasa() {
     document.getElementById("casa").style.borderColor = "yellow";
-    document.getElementById("fora").style.borderColor = "black";
-    document.getElementById("empate").style.borderColor = "black";
+    document.getElementById("fora").style.borderColor = "#333";
+    document.getElementById("empate").style.borderColor = "#333";
 }
 
 function escolheEmpate() {
-    document.getElementById("casa").style.borderColor = "black";
-    document.getElementById("fora").style.borderColor = "black";
+    document.getElementById("casa").style.borderColor = "#333";
+    document.getElementById("fora").style.borderColor = "#333";
     document.getElementById("empate").style.borderColor = "yellow";
 }
 
 function escolheFora() {
-    document.getElementById("casa").style.borderColor = "black";
+    document.getElementById("casa").style.borderColor = "#333";
     document.getElementById("fora").style.borderColor = "yellow";
-    document.getElementById("empate").style.borderColor = "black";
+    document.getElementById("empate").style.borderColor = "#333";
+}
+
+function escolheCasa1(){
+    document.getElementById("casa1").style.borderColor = "yellow";
+    document.getElementById("fora1").style.borderColor = "#333";
+    document.getElementById("empate1").style.borderColor = "#333";
+}
+
+function escolheEmpate1() {
+    document.getElementById("casa1").style.borderColor = "#333";
+    document.getElementById("fora1").style.borderColor = "#333";
+    document.getElementById("empate1").style.borderColor = "yellow";
+}
+
+function escolheFora1() {
+    document.getElementById("casa1").style.borderColor = "#333";
+    document.getElementById("fora1").style.borderColor = "yellow";
+    document.getElementById("empate1").style.borderColor = "#333";
 }
