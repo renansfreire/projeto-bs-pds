7e0cb9426b3e9833760ad21ac8fbf8e864df78d2
Fernandosousa9 <75530766+Fernandosousa9@users.noreply.github.com>
2022-01-12 18:05:15 -0300

    Update aposta.js

--- aposta.js
+++ aposta.js
@@ -33,3 +33,31 @@ function escolheFora1() {
     document.getElementById("fora1").style.borderColor = "yellow";
     document.getElementById("empate1").style.borderColor = "#333";
 }
+
+function id( el ){
+    return document.getElementById( el );
+}
+window.onload = function(){
+
+id('mais').onclick = function(){
+        id('format').value = parseInt( id('format').value )+1;
+}
+id('menos').onclick = function(){
+        if( id('format').value>0 )
+                id('format').value = parseInt( id('format').value )-1;
+    }
+id('mais2').onclick = function(){
+        id('format2').value = parseInt( id('format2').value )+1;
+}
+id('menos2').onclick = function(){
+        if( id('format2').value>0 )
+                id('format2').value = parseInt( id('format2').value )-1;
+    }
+id('finalizar').onclick = function(){
+   id('valor-aposta').value = parseInt(id('valor-aposta').value);
+
+   var calc = (id('format').value*id('format2').value)+1.5;
+
+   id('total').value = 1.5*id('valor-aposta').value*calc;  
+}
+}
